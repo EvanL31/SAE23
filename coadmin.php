865594dc42d93980164400044bf2c9e3a67fddfc
Laffely <laffely@email.com>
2022-06-03 14:25:47 +0200

    Premiere version du projet

---
+++ coadmin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	<html lang="fr">
+		<head>
+			<meta charset="UTF-8" />
+			<title>Connexion Administrateur</title>
+		</head>
+		<body>
+			<p>Connexion administrateur</p>
+		</body>
+	</html>
